db25acd3b509eaf68f9f18b101f83229f054a704
juanesosocasta <86965058+juanesosocasta@users.noreply.github.com>
2021-07-09 08:48:41 -0500

    actualizaciòn

--- Controller/Grafo.py
+++ Controller/Grafo.py
@@ -22,6 +22,47 @@ class Grafo:
             self.verticesList.append(vertice(data))
     def checkVertice(self,data):
         for vertice in self.verticesList:
-            if dato== vertice.getData():
+            if data== vertice.getData():
                 return vertice
-        return m
+        return None
+    def enterEdge(self,source,destinatation,weight):
+        if self.checkEdge(source,destinatation) is None:
+            if self.checkVertice(source) is not None and self.checkVertice(destinatation) is not None:
+                self.edgesList.append(edge(source,destinatation,weight))
+                self.checkVertice(source).getAdjacentList().append(destinatation)
+    def checkEdge(self,source,destinatation):
+        for edge in self.edgesList:
+            if edge.getSource()==source and edge.getDestinatation()== destinatation:
+                return edge
+        return None
+    def deph (self,position,guest_list ):
+        if self.checkVertice(position):
+            if not guest_list:
+                guest_list.append(position)
+            for adjacent in self.checkVertice(position).getAdjacentList():
+                if adyacente not in guest_list:
+                    guest_list.append(adjacent)
+                    guest_list=self.deph(adjacent,guest_list)
+            return guest_list
+        else:
+            return"""El
+        vertice
+        señalado
+        para
+        iniciar
+        el
+        recorrido
+        no
+        existe"""
+
+
+        """"def profundidad(self, dato):
+            if dato in self.listaVisitados:
+                return
+            else:
+                vertice = self.verificarVertice(dato)
+                if vertice is not None:
+                    self.listaVisitados.append(vertice.getDato())
+                    for dato in vertice.getListaAdyacentes():
+                        self.profundidad(dato)
+        """
